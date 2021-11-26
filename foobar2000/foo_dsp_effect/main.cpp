344f298fe78465e1fa0363390374353f4862508a
mudlord <mudlord@protonmail.com>
2021-11-27 10:03:10 +1100

    Go to MSVS 2022, do not past fucking go. Don't collect $200 if you are on ancient systems.

--- foobar2000/foo_dsp_effect/main.cpp
+++ foobar2000/foo_dsp_effect/main.cpp
@@ -1,21 +1,21 @@
 #include "../SDK/foobar2000.h"
 #include "../../../3rdparty-deps/SoundTouch/SoundTouch.h"
 #include "dsp_guids.h"
-#define MYVERSION "0.47"
+#define MYVERSION "0.50 alpha"
 
 static pfc::string_formatter g_get_component_about()
 {
 	pfc::string_formatter about;
-	about << "A special effect DSP for foobar2000.\n\n";
-	about << "Written by mountnside.\n";
-	about << "Twitter: https://twitter.com/mountnside\n";
-	about << "Github:  https://github.com/mountnside\n";
+	about << "A special effect DSP array for foobar2000.\n\n";
+	about << "Written by mudlord.\n";
+	about << "Twitter: https://twitter.com/mudlord7\n";
+	about << "Github:  https://github.com/mudlord\n";
 	about << "\n";
 	about << "Portions by Jon Watte, Jezar Wakefield, Chris Snowhill.\n";
 	about << "Using SoundTouch library version " << SOUNDTOUCH_VERSION << "\n";
 	about << "SoundTouch (c) Olli Parviainen\n";
 	about << "\n";
-	about << "License: https://github.com/mountnside/foobar2000/blob/master/LICENSE.md";
+	about << "License: https://github.com/mudlord/foobar2000/blob/master/LICENSE.md";
 	return about;
 }
 
